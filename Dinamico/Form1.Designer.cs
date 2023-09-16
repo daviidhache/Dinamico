b4a3607c287c277a1dac87bb3af0ab7ec30001c3
daviidhache <socialfreetime@gmail.com>
2023-09-16 21:34:27 +0200

    Agregar archivos de proyecto.

---
+++ Dinamico/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Dinamico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(67, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(321, 219);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox1.Location = new System.Drawing.Point(274, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(447, 111);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(46, 278);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(675, 145);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(315, 176);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(214, 40);
+            this.lbl1.TabIndex = 4;
+            this.lbl1.Text = "Proverbios";
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Dinamico/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dinamico
+{
+
+    /*
+     * FORM que trabaja con un groupbox y un flowlayout.
+     * Para el segundo caso se plantea generar un nº de botones determinados por el usuario
+     * Mostrará proverbios aleatorios de un array con 5 elementos de prueba.
+     */
+    public partial class Form1 : Form
+    {
+        // Array que contendrá 5 mensajes para mostrar en botones.
+       static String[] arrayMensajes = new string[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+              // Método que crea el contenido de los comboBox.
+               cargarColecciones();
+            // Método que carga frases del array.
+                asignarMensajes();
+
+        }
+
+        private void cargarColecciones() {
+            // Cargar la primera colección
+            for (int i = 0; i <= 20; i++) {
+                comboBox1.Items.Add(i);
+            }
+
+            // Cargar segunda coleccion
+
+            for (int i = 0; i <= 20; i++)
+            {
+                comboBox2.Items.Add(i);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                // Limpiamos el group para mostrar cada vez el numero de botones dado por el usuario.
+                
+            groupBox1.Controls.Clear();
+            for (int i = 0; i < Convert.ToInt16(comboBox1.Text); i++) {
+                Button boton = new Button();
+                boton.SetBounds(100 * i, 10, 70, 20);
+                boton.Text = Convert.ToString(i + 1);
+                groupBox1.Controls.Add(boton);
+            }
+           
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*
+             * El flowlayout ajusta de forma automática los elementos dentro del panel.
+             */
+
+            flowLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < Convert.ToInt16(comboBox2.Text); i++)
+            {
+                Button boton = new Button();
+                boton.Click += Boton_Click;
+                boton.Text = Convert.ToString(i+1);
+                flowLayoutPanel1.Controls.Add(boton);
+            }
+        }
+
+
+
+        private void Boton_Click(object sender, EventArgs e)
+
+        {
+            MessageBox.Show(arrayMensajes[aleatorio()], "Proverbios");
+
+           //  MessageBox.Show(Convert.ToString(aleatorio()),"Número aleatorio");
+        }
+
+        private int aleatorio() {
+            Random aleatorio = new Random();
+           return  aleatorio.Next(0, 4);
+            
+        }
+
+        private void asignarMensajes() {
+            arrayMensajes[0] = "El que busca un amigo sin defectos se queda sin amigos.";
+            arrayMensajes[1] = "La gente se arregla todos los días el cabello. ¿Por qué no el corazón?";
+            arrayMensajes[2] = "Si lo que vas a decir no es más bello que el silencio: no lo digas.";
+            arrayMensajes[3] = "La paciencia es un árbol de raíz amarga pero de frutos muy dulces.";
+            arrayMensajes[4] = "Si te caes siete veces, levántate ocho.";
+        }
+
+      
+    }
+}
